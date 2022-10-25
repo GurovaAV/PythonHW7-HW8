c99e93272451d6303c5736b2e06b1066f3850ea9
Anastasiya Gurova <obscured@mail.ru>
2022-10-25 22:53:05 +0300

    Console interface enhanced, exit added

--- controller.py
+++ controller.py
@@ -1,19 +1,37 @@
 import basic as db
 import view as ui
+import log
 
 def menu():
     going=True
     while going:
-        request=['Просмотр справочника; Добавление контакта; Удаление контакта; Поиск контакта; Экспорт в файл']
+        request=['Просмотр справочника;Добавление контакта;Удаление контакта;Поиск контакта;Экспорт в CSV;Экспорт в XML;Выход']
         choice = ui.get_choice(request[0])
         if choice == 1:
             ui.get_look()
+            log.input_write("Просмотрена телефонная книга")
+            break
         elif choice == 2:
             db.get_add()
+            log.input_write("Добавлен контакт")
+            break
         elif choice == 3:
             db.get_remove()
+            log.input_write("Удален контакт")
+            break
         elif choice == 4:
             db.get_find()
+            log.input_write("Выполнен поиск контакта")
+            break
         elif choice == 5:
             db.get_out()
-        input('Нажмите Enter для продолжения')
+            log.input_write("Проведена выгрузка контактов во внешний файл")
+            break
+        elif choice == 6:
+            db.get_out_xml()
+            log.input_write("Проведена выгрузка контактов в XML-формат")
+            break
+        elif choice == 7:
+            print('До новых встреч!')
+            break
+        
